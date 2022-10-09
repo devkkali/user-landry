451ca5d1e6ae3bbc4095fd274b9d22a713d61059
devkkali <33949852+devkkali@users.noreply.github.com>
2022-10-09 18:20:23 +0545

    project setup

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -23,7 +23,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head />
-        <body className='roshan'>
+        <body>
           <Main />
           <NextScript />
         </body>
